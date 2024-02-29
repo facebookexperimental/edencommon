fe13d5e3576a772189c67b1563edaadd44bedad2
John Elliott <jdelliot@meta.com>
2024-02-29 15:43:22 -0800

    Move ImmediateFuture from eden to edencommon
    Summary:
    To support better telemetry and logging in watchman we want to use Eden's components. Lets migrate and detangle the needed pieces.
    
    This change moves ImmediateFuture from eden to edencommon.
    
    Reviewed By: fanzeyi
    
    Differential Revision: D54337631
    
    fbshipit-source-id: 2e09a9e244ef8b8b662959c1b14db3e103f559d0

---
+++ eden/common/utils/ImmediateFuture.cpp
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "eden/common/utils/ImmediateFuture.h"
+
+namespace facebook::eden {
+ImmediateFuture<folly::Unit> makeNotReadyImmediateFuture() {
+  return ImmediateFuture{
+      folly::SemiFuture<folly::Unit>{folly::unit},
+      ImmediateFuture<folly::Unit>::SemiFutureReadiness::LazySemiFuture};
+}
+} // namespace facebook::eden
