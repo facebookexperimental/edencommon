0923e41014670a5c11201bc7067bb2ca02167491
John Elliott <jdelliot@meta.com>
2024-03-01 18:09:03 -0800

    Move FileUtils and PathFuncs from eden to edencommon
    Summary:
    To support better telemetry and logging in watchman we want to use Eden's components. Lets migrate and detangle the needed pieces.
    
    This change moves FileUtils and PathFuncs from eden to edencommon.
    
    NOTE: FileUtils and PathFuncs are mutally dependent so they were migrated togehter. Most of the files touched, however, was due to PathFuncs.
    
    Reviewed By: genevievehelsel
    
    Differential Revision: D54393041
    
    fbshipit-source-id: 6dc404b1b1066704584da512220475e9ee486247

--- eden/common/utils/FileUtils.h
+++ eden/common/utils/FileUtils.h
@@ -7,7 +7,17 @@
 
 #pragma once
 
+#include <folly/Range.h>
+#include <folly/Try.h>
+#include <limits>
+#include <string>
+
+#include "eden/common/utils/FileOffset.h"
+#include "eden/common/utils/Handle.h"
+#include "eden/common/utils/PathFuncs.h"
+
 #ifdef _WIN32
+
 #include <memory>
 #include <system_error>
 
@@ -18,7 +28,64 @@ template <typename T>
 class Try;
 }
 
+#endif
+
 namespace facebook::eden {
+
+/** Read up to num_bytes bytes from the file */
+FOLLY_NODISCARD folly::Try<std::string> readFile(
+    AbsolutePathPiece path,
+    size_t num_bytes = std::numeric_limits<size_t>::max());
+
+/** Write data to the file pointed by path */
+FOLLY_NODISCARD folly::Try<void> writeFile(
+    AbsolutePathPiece path,
+    folly::ByteRange data);
+
+/** Atomically replace the content of the file with data.
+ *
+ * On failure, the content of the file is unchanged.
+ */
+FOLLY_NODISCARD folly::Try<void> writeFileAtomic(
+    AbsolutePathPiece path,
+    folly::ByteRange data);
+
+/**
+ * Read all the directory entry and return their names.
+ *
+ * On non-Windows OS, this is simply a wrapper around
+ * boost::filesystem::directory_iterator.
+ *
+ * On Windows, we have to use something different as Boost will use the
+ * FindFirstFile API which doesn't allow the directory to be opened with
+ * FILE_SHARE_DELETE. This sharing flags allows the directory to be
+ * renamed/deleted while it is being iterated on.
+ */
+FOLLY_NODISCARD folly::Try<std::vector<PathComponent>>
+getAllDirectoryEntryNames(AbsolutePathPiece path);
+
+#ifdef _WIN32
+
+/*
+ * Following is a traits class for File System handles with its handle value and
+ * close function.
+ */
+struct FileHandleTraits {
+  using Type = HANDLE;
+
+  static Type invalidHandleValue() noexcept {
+    return INVALID_HANDLE_VALUE;
+  }
+  static void close(Type handle) noexcept {
+    CloseHandle(handle);
+  }
+};
+
+using FileHandle = HandleBase<FileHandleTraits>;
+/**
+ * For Windows only, returns the file size of the materialized file.
+ */
+folly::Try<FileOffset> getMaterializedFileSize(AbsolutePathPiece pathToFile);
 
 // We declare our own copy here because Ntifs.h is not included in the
 // standard install of the Visual Studio Community compiler.
@@ -70,6 +137,6 @@ using ReparseDataBuffer =
 
 folly::Try<ReparseDataBuffer> getReparseData(HANDLE fd);
 
-} // namespace facebook::eden
-
 #endif
+
+} // namespace facebook::eden
