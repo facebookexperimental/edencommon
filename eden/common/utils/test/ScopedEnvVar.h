7223de300508f2c34a666cdbd0e89370de944763
John Elliott <jdelliot@meta.com>
2024-04-23 20:41:06 -0700

    Move UserInfo from eden to edencommon
    Summary:
    To support better telemetry and logging in watchman we want to use Eden's components. Lets migrate and detangle the needed pieces.
    
    This change moves UserInfo from eden to edencommon.
    
    Reviewed By: kmancini
    
    Differential Revision: D56326986
    
    fbshipit-source-id: 80ff63a33b3f692be8c9a94d38c41409bc1b49f5

---
+++ eden/common/utils/test/ScopedEnvVar.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+#include <folly/Range.h>
+#include <optional>
+
+namespace facebook::eden {
+
+/**
+ * A helper class for manipulating an environment variable,
+ * and restoring it to its original state at the end of the current scope.
+ */
+class ScopedEnvVar {
+ public:
+  explicit ScopedEnvVar(folly::StringPiece name);
+  ~ScopedEnvVar();
+
+  /**
+   * Unset the environment variable.
+   */
+  void unset();
+
+  /**
+   * Set the environment variable
+   */
+  void set(const char* value);
+  void set(const std::string& value);
+  void set(folly::StringPiece value);
+
+ private:
+  /**
+   * The environment variable name.
+   */
+  std::optional<std::string> name_;
+
+  /**
+   * The original value of this environment variable that we should restore it
+   * to on destruction of this ScopedEnvVar.  This will be std::nullopt if the
+   * environment variable was originally unset, and should be unset on
+   * destruction.
+   */
+  std::optional<std::string> origValue_;
+};
+} // namespace facebook::eden
