75557be968c738c8d28ee95839b552b7b40c91a0
Jane Zhang <jazhang@meta.com>
2026-04-27 15:39:13 -0700

    telemetry: Add ODS metric counters
    Summary: Add counters to track success rate, drop rate, and failure rate to help track efficacy perforamnce, and error rate. This way we can spot any regressions or performance degredations and roll back as necessary.
    
    Reviewed By: kavehahmadi60
    
    Differential Revision: D101439046
    
    fbshipit-source-id: 3bd12f4414b6ee41f97209cc9a1e4091efb51160

--- eden/common/telemetry/Stats.h
+++ eden/common/telemetry/Stats.h
@@ -13,6 +13,14 @@ namespace facebook::eden {
 
 struct TelemetryStats : StatsGroup<TelemetryStats> {
   Counter subprocessLoggerFailure{"telemetry.subprocess_logger_failure"};
+  Counter xplatMessagesEnqueued{"telemetry.xplat_messages_enqueued"};
+  Counter xplatMessagesWritten{"telemetry.xplat_messages_written"};
+  Counter xplatMessagesDroppedQueueFull{
+      "telemetry.xplat_messages_dropped_queue_full"};
+  Counter xplatMessagesDroppedShutdown{
+      "telemetry.xplat_messages_dropped_shutdown"};
+  Counter xplatWriteZeroOkRecords{"telemetry.xplat_write_zero_ok_records"};
+  Counter xplatWriteFailures{"telemetry.xplat_write_failures"};
 };
 
 } // namespace facebook::eden
